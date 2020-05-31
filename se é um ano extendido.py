bfe7876876b262e67dcb66b77a01c3447271fba1
DenisLucas <denislucasvalverdesilva@gmail.com>
2020-05-30 22:56:14 -0300

    (concluido)

--- se é um ano extendido.py
+++ se é um ano extendido.py
@@ -1,7 +1,7 @@
 
 def ano_salto(ano):
     anos_salto = bool
-    if int(ano) % 4 == 0 :
+    if int(ano) % 4 == 0:
         if int(ano) % 100 == 0:
             if int(ano) % 400 == 0:
                 anos_salto = True
